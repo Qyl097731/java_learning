db9939a2bc90dbd80f0f1030f6b1d5ed13e4fbe5
qyl <553579048@qq.com>
2022-08-19 10:35:12 +0800

    add codegenerator

---
+++ mockData/src/main/java/com/njxzc/mockdata/MockDataApplication.java
@@ -0,0 +1,13 @@
+package com.njxzc.mockdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockDataApplication.class, args);
+    }
+
+}
