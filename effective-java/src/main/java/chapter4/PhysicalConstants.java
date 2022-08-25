08039e9403ad32d3fd9499ffdebc0f4dc8033475
邱依良 <553579048@qq.com>
2022-08-25 22:27:00 +0800

    增加常量类的写法

---
+++ effective-java/src/main/java/chapter4/PhysicalConstants.java
@@ -0,0 +1,13 @@
+package chapter4;
+
+/**
+ * @description 常量类案例
+ * @date:2022/8/25 22:19
+ * @author: qyl
+ */
+public class PhysicalConstants {
+    private PhysicalConstants(){}
+
+    public static final Integer VALUE = 1;
+    public static final String VALUE_NAME = "VALUE_NAME";
+}
