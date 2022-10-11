a7201d0b9986a06787671293c4f20d86f7161883
邱依良 <553579048@qq.com>
2022-10-11 23:21:29 +0800

    project init

---
+++ restructure/src/main/java/com/nju/restructure/RestructureApplication.java
@@ -0,0 +1,13 @@
+package com.nju.restructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestructureApplication.class, args);
+    }
+
+}
---
+++ restructure/src/test/java/com/nju/restructure/RestructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.restructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
