e3f17a5fac62ad339dd3f9c2b11884d13dc133aa
邱依良 <553579048@qq.com>
2022-10-15 10:31:53 +0800

    add learning notes and demos

---
+++ refactor/src/main/java/com/nju/refactor/chapter6/DateTester.java
@@ -0,0 +1,33 @@
+package com.nju.refactor.chapter6;
+
+import junit.framework.TestCase;
+
+import java.util.Date;
+
+/**
+ * @description
+ * @date:2022/10/15 9:16
+ * @author: qyl
+ */
+public class DateTester {
+    public static void nextDateUpdate(Date arg) {
+        arg.setDate(arg.getDate() + 1);
+        System.out.println("in nextDateUpdate date = " + arg);
+    }
+
+    public static void nextDateReplace(Date arg) {
+        arg = new Date(arg.getYear(),arg.getMonth(),arg.getDate()+1);
+        System.out.println("in nextDateReplace date = " + arg);
+    }
+
+    public static void main(String[] args) {
+        Date date = new Date("1 Apr 98");
+        nextDateUpdate(date);
+        System.out.println("after nextDateUpdate date = " + date);
+
+        Date date1 = new Date("1 Apr 98");
+        nextDateReplace(date1);
+        System.out.println("after nextDateReplace date = " + date1);
+
+    }
+}
