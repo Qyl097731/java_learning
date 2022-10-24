f3475765d707b34266a8e26a150295e2550461ff
邱依良 <553579048@qq.com>
2022-10-24 22:14:43 +0800

    add Template.java for Cf

--- leetcode/src/main/java/templates/Template.java
+++ leetcode/src/main/java/templates/Template.java
@@ -1,20 +1,69 @@
 package templates;
 
+import java.util.*;
 import java.io.*;
 
+import static java.lang.System.*;
+
 /**
- * @description
- * @date:2022/10/23 18:44
- * @author: qyl
+ * @author asus
  */
 public class Template {
-    public static void main(String[] args) throws FileNotFoundException {
-//        BufferedReader f = new BufferedReader(new FileReader("input.txt"));
-//        BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
-        try{
-            System.setIn(new FileInputStream("input"));
-        }catch (Exception e){
+    static FastScanner cin;
+    static PrintWriter cout;
 
+    private static void init() throws IOException {
+        cin = new FastScanner(in);
+        cout = new PrintWriter(out);
+    }
+
+    private static void close() {
+        cout.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        solve();
+        close();
+    }
+
+    private static void solve() throws IOException {
+
+    }
+}
+
+class FastScanner {
+    BufferedReader br;
+    StringTokenizer st = new StringTokenizer("");
+
+    public FastScanner(InputStream s) {
+        br = new BufferedReader(new InputStreamReader(s));
+    }
+
+    public FastScanner(String s) throws FileNotFoundException {
+        br = new BufferedReader(new FileReader(new File(s)));
+    }
+
+    public String next() throws IOException {
+        while (!st.hasMoreTokens()) {
+            try {
+                st = new StringTokenizer(br.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+        return st.nextToken();
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next());
     }
 }
