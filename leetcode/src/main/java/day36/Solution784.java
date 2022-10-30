9075eeb2df2cd9910186964f18919cf1a86019fa
邱依良 <553579048@qq.com>
2022-10-30 15:50:07 +0800

    刷刷水题

---
+++ leetcode/src/main/java/day36/Solution784.java
@@ -0,0 +1,41 @@
+package day36;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @description
+ * @date:2022/10/30 15:27
+ * @author: qyl
+ */
+public class Solution784 {
+    List<String> res = new ArrayList<>();
+
+    public List<String> letterCasePermutation(String s) {
+        dfs(s.toCharArray(), 0, s.length());
+        return res;
+    }
+
+    private void dfs(char[] s, int cur, int len) {
+        while (cur < len && Character.isDigit(s[cur])) {
+            cur++;
+        }
+        if (cur == len) {
+            res.add(new String(s));
+            return;
+        }
+        s[cur] ^= 32;
+        dfs(s, cur + 1, len );
+        s[cur] ^= 32;
+        dfs(s, cur + 1, len);
+    }
+
+    @Test
+    public void test() {
+        System.out.println(letterCasePermutation("C"));
+    }
+}
