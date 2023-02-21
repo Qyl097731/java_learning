8da9d0049dd8237384f73f705c2324ab2068400a
邱依良 <553579048@qq.com>
2023-02-21 23:44:32 +0800

    刷水题

---
+++ leetcode/src/main/java/daily/Solution1326.java
@@ -0,0 +1,18 @@
+package daily;
+
+import java.util.Arrays;
+
+/**
+ * @description
+ * @date 2023/2/21 23:33
+ * @author: qyl
+ */
+public class Solution1326 {
+    public int minTaps(int n, int[] ranges) {
+        int[] dp = new int[n + 1];
+        Arrays.fill (dp, Integer.MAX_VALUE);
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+}
