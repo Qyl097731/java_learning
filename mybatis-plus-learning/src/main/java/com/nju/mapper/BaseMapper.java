1a006ead3e4f21b0966453b6cb3aa20b3627d3f1
邱依良 <553579048@qq.com>
2023-03-08 01:40:55 +0800

    es review

---
+++ mybatis-plus-learning/src/main/java/com/nju/mapper/BaseMapper.java
@@ -0,0 +1,12 @@
+package com.nju.mapper;
+
+/**
+ * @description BaseMapper
+ * @date 2023/3/3 0:59
+ * @author: qyl
+ */
+public class BaseMapper {
+    public static void main(String[] args) {
+
+    }
+}
