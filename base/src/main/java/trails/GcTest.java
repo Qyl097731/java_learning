2ae8b3e7e951f04df7222326da65b73e682a518a
qiuyiliang <qiuyiliang@meituan.com>
2023-07-07 15:20:19 +0800

    【add] add gc test

---
+++ base/src/main/java/trails/GcTest.java
@@ -0,0 +1,75 @@
+package trails;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.Thread.sleep;
+
+/**
+ * @author qiuyiliang
+ */
+public class GcTest {
+    public static void main(String[] args) {
+        List<Order> orderList = new ArrayList<>();
+        for (int i = 0; i < 7000000; i++) {
+            Order order = new Order();
+            order.setId(i);
+            order.setGid(i);
+            order.setOid(i);
+            orderList.add(order);
+        }
+        List<Order> filteredOrders = filterOrder(orderList);
+
+        List<Order> statisticOrders = statisticOrder(filteredOrders);
+    }
+
+    private static List<Order> statisticOrder(List<Order> filteredOrders) {
+        List<Order> statisticOrders = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 300; j++) {
+                statisticOrders.add(filteredOrders.get(i));
+            }
+            sleep(1000);
+        }
+        return statisticOrders;
+    }
+
+    private static List<Order> filterOrder(List<Order> orderList) {
+        List<Order> filteredOrders = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            for (int j = 0; j < 300; j++) {
+                filteredOrders.add(orderList.get(i));
+            }
+        }
+        return filteredOrders;
+    }
+}
+class Order{
+    private int id;
+    private int oid;
+    private int gid;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
+
+    public int getGid() {
+        return gid;
+    }
+
+    public void setGid(int gid) {
+        this.gid = gid;
+    }
+}
